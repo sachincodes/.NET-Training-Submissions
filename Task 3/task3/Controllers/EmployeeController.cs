7ae71c22576d508a27a867d1af7db69d86195c27
nishant <nishant@DESKTOP-60HK1IC>
2023-07-07 14:34:42 +0530

    added task

---
+++ Task 3/task3/Controllers/EmployeeController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using task3.Models;
+
+namespace task3.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private static Dictionary<int, Employee> employees = new Dictionary<int, Employee>();
+        private static int nextId = 1;
+
+        public ActionResult Index()
+        {
+            return View(employees.Values);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Employee employee)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    employee.Id = nextId;
+                    employees[nextId] = employee;
+                    nextId++;
+                    return RedirectToAction("Index");
+                }
+                return View(employee);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                if (employees.ContainsKey(id))
+                {
+                    return View(employees[id]);
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Employee employee)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    if (employees.ContainsKey(employee.Id))
+                    {
+                        employees[employee.Id] = employee;
+                    }
+                    return RedirectToAction("Index");
+                }
+
+                return View(employee);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                if (employees.ContainsKey(id))
+                {
+                    return View(employees[id]);
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            try
+            {
+                if (employees.ContainsKey(id))
+                {
+                    employees.Remove(id);
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Task 3/task3/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace task3.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter the employee's name.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter the employee's department.")]
+        public string Department { get; set; }
+
+        [Required(ErrorMessage = "Please enter the employee's salary.")]
+        [Range(0, double.MaxValue, ErrorMessage = "Salary must be a positive number.")]
+        public double Salary { get; set; }
+    }
+}
