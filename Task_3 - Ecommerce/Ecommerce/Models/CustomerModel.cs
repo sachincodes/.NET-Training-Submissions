51828370dafd2719187e81b2294b7e3af34f8856
sumit-evontech <panwar.sumit@evontech.com>
2023-07-11 17:36:14 +0530

    Model created

---
+++ Task_3 - Ecommerce/Ecommerce/Models/CustomerModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Ecommerce.Models
+{
+    public class CustomerModel
+    {
+        public string? CustomerId { get; set; }
+        public string? CustomerName { get; set; }
+        public string? CustomerEmail { get; set; }
+        public string? CustomerPhone { get; set; }
+        public string? CustomerAddress { get; set;}
+
+        public CustomerModel()
+        {
+            CustomerId = Guid.NewGuid().ToString();
+        }
+    }
+}
---
+++ Task_3 - Ecommerce/Ecommerce/Models/ProductModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Ecommerce.Models
+
+
+{
+    public class ProductModel
+    {
+        public string? ProductId { get; set; }
+        public string? ProductName { get; set; }
+        public string? ProductDescription { get; set; }
+        public string? ProductImage { get; set; }
+        public float? ProductPrice { get; set; }
+
+        public ProductModel()
+        {
+            ProductId = Guid.NewGuid().ToString();
+        }
+
+    }
+}
