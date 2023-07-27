770c66f93dd054d732831ac3d36f7931818e988f
sumit-evontech <panwar.sumit@evontech.com>
2023-07-27 12:16:05 +0530

    social media posts ajax

---
+++ Task_5 - SocialMedia-Ajax/SocialMedia-Ajax/Controllers/PostsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SocialMedia_Ajax.Models;
+
+namespace SocialMedia_Ajax.Controllers
+{
+    public class PostsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public List<SocialMediaPostModel> GetPosts(int limit  = 5, int start = 0)
+        {
+            List<SocialMediaPostModel> posts = SocialMedia_Ajax.Data.postsdata.getAllPosts(limit, start);
+            return posts;
+        }
+
+    }
+}
---
+++ Task_5 - SocialMedia-Ajax/SocialMedia-Ajax/Data/postsdata.cs
@@ -0,0 +1,21 @@
+﻿using SocialMedia_Ajax.Models;
+
+namespace SocialMedia_Ajax.Data
+{
+    public static class postsdata
+    {
+        public static List<SocialMediaPostModel> getAllPosts(int limit, int start)
+        {
+            List<SocialMediaPostModel> posts = new();
+            for (int i = start; i < 100; i++)
+            {
+                if(--limit < 0)
+                {
+                    break;
+                }
+                posts.Add(new SocialMediaPostModel(i+1));
+            }
+            return posts;
+        }
+    }
+}
