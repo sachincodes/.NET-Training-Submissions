0c119cbb5680c77bd278bfe33a2cc83794fe84ca
shrish <shrish@tvsmotor.com>
2023-07-28 09:45:04 +0530

    ajax call task

---
+++ Assigments/Ajax_Tasks/Ajax_Tasks/Models/SocialFeed.cs
@@ -0,0 +1,10 @@
+﻿namespace Ajax_Tasks.Models
+{
+    public class SocialFeed
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
